757cbebc95c099e578e1a954e3f8b90035040e3b
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-12 04:07:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -30,8 +30,8 @@ class Pokemon
     SQL
     
     row = db.execute(sql, id)
-    self.new(:id => row[0], :name => row[1], :type => row[2], :db => db)
-    
+    new_object = self.new(:id => row[0], :name => row[1], :type => row[2], :db => db)
+    new_object
   end
   
   
