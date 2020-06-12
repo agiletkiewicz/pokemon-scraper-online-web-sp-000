764038cdb1d8fae0242c4a3f63ba03a9355cf9cd
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-12 04:11:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Pokemon
   
   attr_accessor :name, :type, :db 
@@ -32,6 +34,7 @@ class Pokemon
     row = db.execute(sql, id)
     new_object = self.new(:id => row[0], :name => row[1], :type => row[2], :db => db)
     new_object
+    binding.pry
   end
   
   
